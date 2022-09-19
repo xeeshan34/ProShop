1131020a75ff90258c5fcc813b266cffc431c0cd
xeeshan34 <zeeshanghulamnabi81@gmail.com>
2022-09-19 03:37:58 -0700

    after some time

---
+++ backend/seeder.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import users from "./data/users.js";
+import product from "./data/products.js";
+import User from "./models/userModel.js";
+import Product from "./models/productModel.js";
+import Order from "./models/orderModel.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+  try {
+    await User.deleteMany();
+    await Product.deleteMany();
+    await Order.deleteMany();
+
+    const createUsers = await User.insertMany(users);
+    const adminUser = createUsers[0]._id;
+    const sampleProducts = product.map((product) => {
+      console.log("");
+      return { ...product, user: adminUser };
+    });
+
+    await Product.insertMany(sampleProducts);
+    console.log("Data Imported");
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await User.deleteMany();
+    await Product.deleteMany();
+    await Order.deleteMany();
+
+    console.log("Data Destroyed");
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === "-d") {
+  destroyData();
+} else {
+  importData();
+}
